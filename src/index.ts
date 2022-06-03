6d6b554724952da2db976f4154c1f7fde54e227f
A90G <104040612+A90G@users.noreply.github.com>
2022-06-03 18:42:00 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/*•El DT de los infantiles del equipo de fútbol desea
+saber el promedio de la edad de los chicos
+• La edad de los chicos va de 3 a 7 años. Las
+edades son cargadas al azar (use la función
+aleatorio(menorValor, mayorValor), es decir
+aleatorio(3,7))
+• Muestre todas las edades y el promedio de las
+mismas*/
